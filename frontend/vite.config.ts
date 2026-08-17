4cf90ad4c1b459df6c267d8d59efff9bf1677de8
Arvind <arvindkumar.ms@ascendion.com>
2026-08-17 10:56:17 +0530

    updated vite.config.ts to allow ngrok

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Option A: Allow your specific ngrok domain
+    allowedHosts: ['unempirical-hermila-enunciatory.ngrok-free.dev'],
+  }
 })
+
+
