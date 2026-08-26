56f061a6b414595e3402a67ddcfd24206d2c12c4
Arvind <arvindkumar.ms@ascendion.com>
2026-08-26 10:32:39 +0530

    Updated TODO

--- TODO.py
+++ TODO.py
@@ -1,5 +1,3 @@
-# Make Dashboards clearly convey information
 # Make the chatbot better - AUTOCOMPLETE
 # Implement workspace by getting the userid
-# Mobile fix
 # Visual makeover, anything like transitions, animations
